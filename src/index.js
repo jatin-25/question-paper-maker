d658005f3a40f4652c0923c4021f474f12f884b1
jatinkh25 <jatinkhatri25.jk@gmail.com>
2022-07-05 14:23:40 +0530

    netlify bug resolved

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+import App from './app.js';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './store/reducers/auth';
+import authReducer from './store/reducers/auth.js';
 import { BrowserRouter as Routers } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
